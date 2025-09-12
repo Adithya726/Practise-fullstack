c81bb5970ccaac4cac2fda80df648b6f4b62ec1b
Adithya726 <163718794+Adithya726@users.noreply.github.com>
2025-09-12 21:57:47 +0530

    Updated PractisebackendApplication.java

--- practisebackend/src/main/java/com/practise/edu/PractisebackendApplication.java
+++ practisebackend/src/main/java/com/practise/edu/PractisebackendApplication.java
@@ -2,13 +2,15 @@ package com.practise.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PractisebackendApplication {
+public class PractisebackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PractisebackendApplication.class, args);
 		System.out.println("Project is Running...");
+		
 	}
 
 }
